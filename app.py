6439909276dad00f43dd86b692a6cc7543a92f31
lutpio <ahmadlutfi606@gmail.com>
2023-11-07 20:22:20 +0800

    first commit

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
 
-app = Flask(__name__)
 
-client = MongoClient(
-    "mongodb+srv://ahmadlutfi606:wolfattax@cluster0.xctxali.mongodb.net/?retryWrites=true&w=majority"
-)
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+# client = MongoClient(
+#     "mongodb+srv://ahmadlutfi606:wolfattax@cluster0.xctxali.mongodb.net/?retryWrites=true&w=majority"
+# )
+# db = client.dbsparta
+
+app = Flask(__name__)
 
 
 @app.route("/")
